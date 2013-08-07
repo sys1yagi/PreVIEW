e55dfd37e6115ac8b77f3a033fb9af3834ef93b5
yagi <sylc.yagi@gmail.com>
2013-08-07 22:09:26 +0900

    text areaとpreviewの高さを揃えた

--- public/javascripts/pages/index.js
+++ public/javascripts/pages/index.js
@@ -6,6 +6,15 @@ define(
     ],
     function(compile, observer){
         function initialize(){
+            var source= $("#source_code");
+            var preview= $("#preview");
+            function fitHeight(){
+                source.height($(window).height()-source.offset().top-50);
+                preview.height($(window).height()-preview.offset().top-50);
+            }
+            $(window).on("resize", fitHeight);
+            fitHeight();
+
             compile.attachTo("#preview", {"source":"#source_code", "destination":"#preview"});
             observer.attachTo("#source_code", {});
         }
