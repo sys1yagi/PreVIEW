6e181124fee8e220ad130d3e1ab8b3253b062876
yagi <sylc.yagi@gmail.com>
2013-08-10 23:46:56 +0900

    行番号表示を追加

--- public/javascripts/domain/index/source_code_observer.js
+++ public/javascripts/domain/index/source_code_observer.js
@@ -9,6 +9,83 @@ define(
     function (component) {
         return component(SourceCodeObserver);
         function SourceCodeObserver() {
+
+            var lineObjOffsetTop = 2;
+            function createTextAreaWithLines(id) {
+                var el = document.createElement('DIV');
+                var ta = document.getElementById(id);
+                ta.parentNode.insertBefore(el, ta);
+                el.appendChild(ta);
+
+                el.className = 'textAreaWithLines';
+                el.style.width = (ta.offsetWidth + 30) + 'px';
+                ta.style.position = 'absolute';
+                ta.style.left = '30px';
+                el.style.height = (ta.offsetHeight + 2) + 'px';
+                el.style.overflow = 'hidden';
+                el.style.position = 'relative';
+                el.style.width = (ta.offsetWidth + 30) + 'px';
+                var lineObj = document.createElement('DIV');
+                lineObj.style.position = 'absolute';
+                lineObj.style.top = lineObjOffsetTop + 'px';
+                lineObj.style.left = '0px';
+                lineObj.style.width = '27px';
+                el.insertBefore(lineObj, ta);
+                lineObj.style.textAlign = 'right';
+                lineObj.className = 'lineObj';
+
+                ta.onkeydown = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onmousedown = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onscroll = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onblur = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onfocus = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onmouseover = function () {
+                    positionLineObj(lineObj, ta);
+                };
+                ta.onkeyup = function(){
+                    updateLineCount(lineObj, id);
+                }
+                updateLineCount(lineObj, id);
+            }
+            function positionLineObj(obj, ta) {
+                obj.style.top = (ta.scrollTop * -1 + lineObjOffsetTop) + 'px';
+            }
+
+            //line counts
+            var lineCount = 0;
+            function updateLineCount(lineObj, id){
+                var ta = document.getElementById(id);
+                var value = ta.value;
+                var num1 = value.match(/\r\n/g); //IE
+                var num2 = value.match(/\n/g);   //Chrome, Firefox
+                var count = 0;
+                if(num1 != null){
+                    count = num1.length+1;
+                }
+                else if(num2 != null){
+                    count = num2.length+1;
+                }
+                if(lineCount != count){
+                    var string = '';
+                    for (var no = 1; no <= count; no++) {
+                        if (string.length > 0)string = string + '<br>';
+                        string = string + no;
+                    }
+                    lineCount = count;
+                    lineObj.innerHTML = string;
+                }
+            }
+
             this.after("initialize", function () {
                 var self = this;
                 var timer = null;
@@ -26,6 +103,7 @@ define(
                         }
                     }, 500);
                 });
+                createTextAreaWithLines(this.node.id);
             });
         }
     }
